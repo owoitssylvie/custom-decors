9736dec5b9d50938b3bb1b66d77522d9f1dfa548
owoitssylvie <sylviarose4ef@gmail.com>
2024-08-02 16:38:34 +0100

    Add files via upload

---
+++ js/translations.js
@@ -0,0 +1,25 @@
+const translations = {
+    en: {
+        title: "Custom Avatar Decors",
+        description: `The largest collection of Avatar Decorations for the Decor Vencord Plugin.<br>
+        You can download Vencord <a href="https://vencord.dev/">here</a>. After Vencord is installed, go to Discord Settings -> Plugins and enable Decor!<br>
+        You'll need to change the metadata of each decoration by reuploading to <a href="https://www.strerr.com/en/md5_hash_chanager.html">this site</a> and then re-downloading.`,
+        newReleasesTitle: "New Releases:",
+        download: "Download",
+        additionalText1: "This website is non-commercial, no ads and open source.",
+        additionalText2: "As such, all images hosted on this site are licensed under fair use. They were sourced via public domain.",
+        creditsLink: "Check out the credits"
+    },
+    es: {
+        title: "Decoraciones de Avatar Personalizadas",
+        description: `La colección más grande de Decoraciones de Avatar para el Plugin Decor Vencord.<br>
+        Puedes descargar Vencord <a href="https://vencord.dev/">aquí</a>. Después de instalar Vencord, ve a Configuración de Discord -> Plugins y habilita Decor!<br>
+        Necesitarás cambiar los metadatos de cada decoración volviéndolos a cargar en <a href="https://www.strerr.com/en/md5_hash_chanager.html">este sitio</a> y luego descargándolos nuevamente.`,
+        newReleasesTitle: "Nuevos Lanzamientos:",
+        download: "Descargar",
+        additionalText1: "Este sitio web es no comercial, sin anuncios y de código abierto.",
+        additionalText2: "Por lo tanto, todas las imágenes alojadas en este sitio están licenciadas bajo uso justo. Fueron obtenidas a través del dominio público.",
+        creditsLink: "Consulta los créditos"
+    }
+    // Add more languages as needed
+};
