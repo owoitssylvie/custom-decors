1592c95fbc9a1069282260a64d5819d1622a43cc
owoitssylvie <sylviarose4ef@gmail.com>
2024-08-03 07:03:34 +0100

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -3,6 +3,7 @@ const translations = {
         title: "Custom Avatar Decors",
         description: 'The largest collection of Avatar Decorations for the Decor Vencord Plugin. You can download Vencord from <a href="https://vencord.dev/download/">here</a>. After installing Vencord, go to Discord settings -> Plugins and enable decor! You will need to change the metadata for each decoration by reloading the image on <a href="https://www.strerr.com/en/md5_hash_chanager.html">this site</a> and then re-downloading it.',
         newReleasesTitle: "New Releases:",
+        download: "Download",
         additionalText1: "This site is non-commercial and ad-free, and it is open-source.",
         additionalText2: "As such, all images hosted on this site are licensed under fair use. They have been obtained via the public domain.",
         creditsLink: "Check the credits"
@@ -11,6 +12,7 @@ const translations = {
         title: "افكتات افتار مخصصه",
         description: 'أكبر مجموعة من ديكورات الصور الرمزية لبرنامج يمكنك تنزيل برنامج فينكورد <a href="https://vencord.dev/download/">من هنا</a> . بعد تثبيت فينكورد ، انتقل إلى إعدادات الديسكورد -> المكونات الإضافية وقم بتمكين الديكور! ستحتاج إلى تغيير البيانات الوصفية لكل زخرفة عن طريق إعادة التحميل <a href="https://www.strerr.com/en/md5_hash_chanager.html">الصوره في هذا الموقع</a> ثم إعادة التنزيل',
         newReleasesTitle: ":اصدارات جديده",
+        download: "تحميل",
         additionalText1: "هذا الموقع غير تجاري ولا يحتوي على إعلانات ومفتوح المصدر.",
         additionalText2: "وعلى هذا النحو، فإن جميع الصور المستضافة على هذا الموقع مرخصة بموجب الاستخدام العادل. لقد تم الحصول عليها عبر المجال العام.",
         creditsLink: "تحقق من الاعتمادات"
@@ -19,6 +21,7 @@ const translations = {
         title: "Decoraciones de Avatar Personalizadas",
         description: 'La colección más grande de decoraciones de avatar para el Plugin Decor Vencord. Puedes descargar Vencord desde <a href="https://vencord.dev/download/">aquí</a>. Después de instalar Vencord, ve a la configuración de Discord -> Plugins y habilita la decoración! Necesitarás cambiar los metadatos de cada decoración recargando la imagen en <a href="https://www.strerr.com/en/md5_hash_chanager.html">este sitio</a> y luego volviéndola a descargar.',
         newReleasesTitle: "Nuevos Lanzamientos:",
+        download: "Descargar",
         additionalText1: "Este sitio es no comercial y sin anuncios, y es de código abierto.",
         additionalText2: "Como tal, todas las imágenes alojadas en este sitio están licenciadas bajo uso justo. Se han obtenido a través del dominio público.",
         creditsLink: "Ver los créditos"
@@ -27,6 +30,7 @@ const translations = {
         title: "Décorations d'Avatar Personnalisées",
         description: 'La plus grande collection de décorations d\'avatar pour le Plugin Decor Vencord. Vous pouvez télécharger Vencord depuis <a href="https://vencord.dev/download/">ici</a>. Après avoir installé Vencord, allez dans les paramètres de Discord -> Plugins et activez la décoration! Vous devrez changer les métadonnées de chaque décoration en rechargeant l\'image sur <a href="https://www.strerr.com/en/md5_hash_chanager.html">ce site</a> puis en la téléchargeant à nouveau.',
         newReleasesTitle: "Nouvelles Sorties:",
+        download: "Télécharger",
         additionalText1: "Ce site est non commercial et sans publicité, et il est open-source.",
         additionalText2: "Ainsi, toutes les images hébergées sur ce site sont licenciées sous l'utilisation équitable. Elles ont été obtenues via le domaine public.",
         creditsLink: "Voir les crédits"
@@ -35,6 +39,7 @@ const translations = {
         title: "Benutzerdefinierte Avatar-Dekorationen",
         description: 'Die größte Sammlung von Avatar-Dekorationen für das Decor Vencord Plugin. Du kannst Vencord von <a href="https://vencord.dev/download/">hier</a> herunterladen. Nach der Installation von Vencord, gehe zu den Discord-Einstellungen -> Plugins und aktiviere die Dekoration! Du musst die Metadaten jeder Dekoration ändern, indem du das Bild auf <a href="https://www.strerr.com/en/md5_hash_chanager.html">dieser Seite</a> neu lädst und dann erneut herunterlädst.',
         newReleasesTitle: "Neuerscheinungen:",
+        download: "Herunterladen",
         additionalText1: "Diese Seite ist nicht kommerziell und werbefrei und ist Open-Source.",
         additionalText2: "Als solche sind alle auf dieser Seite gehosteten Bilder unter Fair Use lizenziert. Sie wurden über die Public Domain bezogen.",
         creditsLink: "Credits anzeigen"
