ca063d66fa7789621a76f7ff7b5930ab8d451dfc
owoitssylvie <sylviarose4ef@gmail.com>
2024-08-06 15:52:17 +0100

    Add files via upload

---
+++ js/translations_creators.js
@@ -0,0 +1,122 @@
+const translations = {
+    en: {
+        title: "Creators Page",
+        description: 'The Decors on this page are provided by their artists and as such are more special and unique than the main page collection.<br> Remember, due to how Decor stores its files, you will need to change the metadata of each downloaded decor before you can use it yourself.<br> You can do that by visiting <a href="https://www.strerr.com/en/md5_hash_chanager.html">this site</a> and then re-downloading the image. If it still fails, try <a href="https://ezgif.com/resize">this site instead</a> and making it like 99% size.',
+        newReleasesTitle: "New Releases:",
+        download: "Download",
+        additionalText1: "This site is non-commercial and ad-free, and it is open-source.",
+        additionalText2: "As such, all images hosted on this site are licensed under fair use. They have been obtained via the public domain.",
+        creditsLink: "Back to Home",
+        creatorsCreditsLink: "Creators Credit"
+    },
+    uk: {
+        title: "Creators Page Innit Bruv",
+        description: 'The Decors on this page are provided by their artists and as such are more special and unique than the main page collection.<br> Remember, due to how Decor stores its files, you will need to change the metadata of each downloaded decor before you can use it yourself.<br> You can do that by visiting <a href="https://www.strerr.com/en/md5_hash_chanager.html">this site</a> and then re-downloading the image. If it still fails, try <a href="https://ezgif.com/resize">this site instead</a> and making it like 99% size.',
+        newReleasesTitle: "New Releases:",
+        download: "Nick It",
+        additionalText1: "This site is non-commercial and baked bean-free, and it is open-sauce.",
+        additionalText2: "As such, all images hosted on this site are licensed under fair use (Just like the British Museum???). They have been obtained via the public domain. (Suuuuuure..)",
+        creditsLink: "Back to Home",
+        creatorsCreditsLink: "Creators Credit"
+    },
+    it: {
+        title: "Pagina dei Creatori",
+        description: 'I Decor su questa pagina sono forniti dai loro artisti e come tali sono più speciali e unici rispetto alla collezione della pagina principale.<br> Ricorda, a causa del modo in cui Decor memorizza i suoi file, dovrai cambiare i metadati di ogni decor scaricato prima di poterlo utilizzare tu stesso.<br> Puoi farlo visitando <a href="https://www.strerr.com/en/md5_hash_chanager.html">questo sito</a> e poi riscaricando l\'immagine. Se ancora non funziona, prova <a href="https://ezgif.com/resize">questo sito</a> e ridimensiona l\'immagine al 99%.',
+        newReleasesTitle: "Nuove Uscite:",
+        download: "Scaricamento",
+        additionalText1: "Questo sito è non commerciale e senza pubblicità, ed è open-source.",
+        additionalText2: "Tutte le immagini ospitate su questo sito sono concesse in licenza per uso legittimo. Sono state ottenute tramite il dominio pubblico.",
+        creditsLink: "Torna alla Home",
+        creatorsCreditsLink: "Crediti dei Creatori"
+    },
+    ar: {
+        title: "صفحة المبدعين",
+        description: 'الزخارف الموجودة في هذه الصفحة مقدمة من فنانيها وبالتالي فهي أكثر تميزًا وفريدة من نوعها مقارنة بمجموعة الصفحة الرئيسية.<br> تذكر أنه بسبب كيفية تخزين Decor لملفاته، ستحتاج إلى تغيير البيانات الوصفية لكل زخرفة تم تنزيلها قبل أن تتمكن من استخدامها بنفسك.<br> يمكنك القيام بذلك عن طريق زيارة <a href="https://www.strerr.com/en/md5_hash_chanager.html">هذا الموقع</a> ثم إعادة تنزيل الصورة. إذا لم ينجح ذلك، جرب <a href="https://ezgif.com/resize">هذا الموقع</a> وجعلها بحجم 99٪.',
+        newReleasesTitle: "إصدارات جديدة:",
+        download: "تحميل",
+        additionalText1: "هذا الموقع غير تجاري وخالٍ من الإعلانات، وهو مفتوح المصدر.",
+        additionalText2: "وبالتالي، جميع الصور المستضافة على هذا الموقع مرخصة للاستخدام العادل. تم الحصول عليها من خلال المجال العام.",
+        creditsLink: "العودة إلى الصفحة الرئيسية",
+        creatorsCreditsLink: "إعتمادات المبدعين"
+    },
+    es: {
+        title: "Página de Creadores",
+        description: 'Las decoraciones en esta página son proporcionadas por sus artistas y como tal son más especiales y únicas que la colección de la página principal.<br> Recuerda, debido a cómo Decor almacena sus archivos, necesitarás cambiar los metadatos de cada decoración descargada antes de poder usarla tú mismo.<br> Puedes hacerlo visitando <a href="https://www.strerr.com/en/md5_hash_chanager.html">este sitio</a> y luego volviendo a descargar la imagen. Si aún falla, prueba <a href="https://ezgif.com/resize">este sitio</a> y hazla del 99% del tamaño.',
+        newReleasesTitle: "Nuevos Lanzamientos:",
+        download: "Descargar",
+        additionalText1: "Este sitio es no comercial y sin publicidad, y es de código abierto.",
+        additionalText2: "Como tal, todas las imágenes alojadas en este sitio están licenciadas para uso justo. Han sido obtenidas a través del dominio público.",
+        creditsLink: "Volver a Inicio",
+        creatorsCreditsLink: "Créditos de los Creadores"
+    },
+    fr: {
+        title: "Page des Créateurs",
+        description: 'Les décorations sur cette page sont fournies par leurs artistes et sont donc plus spéciales et uniques que la collection de la page principale.<br> N\'oubliez pas, en raison de la manière dont Decor stocke ses fichiers, vous devrez changer les métadonnées de chaque décoration téléchargée avant de pouvoir l\'utiliser vous-même.<br> Vous pouvez le faire en visitant <a href="https://www.strerr.com/en/md5_hash_chanager.html">ce site</a> puis en retéléchargeant l\'image. Si cela échoue encore, essayez <a href="https://ezgif.com/resize">ce site</a> et réduisez-le à 99% de sa taille.',
+        newReleasesTitle: "Nouvelles Sorties:",
+        download: "Télécharger",
+        additionalText1: "Ce site est non commercial et sans publicité, et il est open-source.",
+        additionalText2: "En conséquence, toutes les images hébergées sur ce site sont sous licence d'utilisation équitable. Elles ont été obtenues via le domaine public.",
+        creditsLink: "Retour à l'accueil",
+        creatorsCreditsLink: "Crédits des Créateurs"
+    },
+    de: {
+        title: "Ersteller Seite",
+        description: 'Die Dekorationen auf dieser Seite werden von ihren Künstlern bereitgestellt und sind daher spezieller und einzigartiger als die Sammlung auf der Hauptseite.<br> Denken Sie daran, aufgrund der Art und Weise, wie Decor seine Dateien speichert, müssen Sie die Metadaten jeder heruntergeladenen Dekoration ändern, bevor Sie sie selbst verwenden können.<br> Sie können dies tun, indem Sie <a href="https://www.strerr.com/en/md5_hash_chanager.html">diese Seite</a> besuchen und dann das Bild erneut herunterladen. Wenn es immer noch fehlschlägt, versuchen Sie <a href="https://ezgif.com/resize">diese Seite</a> und machen Sie es zu 99% Größe.',
+        newReleasesTitle: "Neuerscheinungen:",
+        download: "Herunterladen",
+        additionalText1: "Diese Seite ist nicht kommerziell und werbefrei und sie ist open-source.",
+        additionalText2: "Daher sind alle Bilder auf dieser Seite unter fair use lizenziert. Sie wurden über die Public Domain bezogen.",
+        creditsLink: "Zurück zur Startseite",
+        creatorsCreditsLink: "Künstlerische Credits"
+    },
+    tr: {
+        title: "Yaratıcılar Sayfası",
+        description: 'Bu sayfadaki Dekorlar sanatçıları tarafından sağlanmakta olup, ana sayfa koleksiyonundan daha özel ve benzersizdir.<br> Decor dosyalarını nasıl depoladığı nedeniyle, indirilen her dekorun meta verilerini değiştirmeden kendiniz kullanamazsınız.<br> Bunu yapabilirsiniz <a href="https://www.strerr.com/en/md5_hash_chanager.html">bu siteyi</a> ziyaret ederek ve ardından resmi yeniden indirerek. Eğer hala başarısız olursa, <a href="https://ezgif.com/resize">bu siteyi</a> deneyin ve resmi %99 boyutunda yapın.',
+        newReleasesTitle: "Yeni Çıkışlar:",
+        download: "İndir",
+        additionalText1: "Bu site ticari değildir ve reklamsızdır, ayrıca açık kaynaklıdır.",
+        additionalText2: "Bu nedenle, bu sitede barındırılan tüm resimler adil kullanım lisansına sahiptir. Kamu malı aracılığıyla elde edilmiştir.",
+        creditsLink: "Ana Sayfaya Dön",
+        creatorsCreditsLink: "Yaratıcı Kredileri"
+    },
+    ro: {
+        title: "Pagina Creatorilor",
+        description: 'Decorurile de pe această pagină sunt furnizate de artiștii lor și, ca atare, sunt mai speciale și unice decât colecția de pe pagina principală.<br> Amintiți-vă, din cauza modului în care Decor își stochează fișierele, va trebui să schimbați metadatele fiecărei decorațiuni descărcate înainte de a o putea folosi personal.<br> Puteți face acest lucru vizitând <a href="https://www.strerr.com/en/md5_hash_chanager.html">acest site</a> și apoi descărcând din nou imaginea. Dacă încă nu funcționează, încercați <a href="https://ezgif.com/resize">acest site</a> și faceți-o la 99% din dimensiune.',
+        newReleasesTitle: "Lansări Noi:",
+        download: "Descarcă",
+        additionalText1: "Acest site nu este comercial și fără reclame și este open-source.",
+        additionalText2: "Prin urmare, toate imaginile găzduite pe acest site sunt licențiate pentru utilizare echitabilă. Au fost obținute prin domeniul public.",
+        creditsLink: "Înapoi la Acasă",
+        creatorsCreditsLink: "Credite pentru Creatori"
+    },
+    lt: {
+        title: "Kūrėjų puslapis",
+        description: 'Šioje puslapyje esantys dekorai yra pateikti jų menininkų, todėl jie yra ypatingesni ir unikalūs nei pagrindinio puslapio kolekcija.<br> Atminkite, kad dėl to, kaip Decor saugo savo failus, jums reikės pakeisti kiekvieno atsisiųsto dekoro meta duomenis, prieš galėdami jį naudoti patys.<br> Tai galite padaryti apsilankę <a href="https://www.strerr.com/en/md5_hash_chanager.html">šioje svetainėje</a> ir vėl atsisiųsdami vaizdą. Jei tai vis dar neveikia, išbandykite <a href="https://ezgif.com/resize">šią svetainę</a> ir sumažinkite ją iki 99% dydžio.',
+        newReleasesTitle: "Nauji Išleidimai:",
+        download: "Atsisiųsti",
+        additionalText1: "Ši svetainė yra nekomercinė ir be reklamos, taip pat yra atvirojo kodo.",
+        additionalText2: "Taigi, visos šioje svetainėje talpinamos nuotraukos yra licencijuotos pagal sąžiningo naudojimo sąlygas. Jos buvo gautos iš viešojo domeno.",
+        creditsLink: "Grįžti į Pradžią",
+        creatorsCreditsLink: "Kūrėjų Kredito"
+    },
+    hu: {
+        title: "Készítők Oldala",
+        description: 'Ezen az oldalon található dekorációk a művészeik által biztosítottak, és így különlegesebbek és egyedibbek, mint a főoldalon található gyűjtemény.<br> Ne feledje, hogy mivel a Decor így tárolja a fájlokat, módosítania kell minden letöltött dekoráció metaadatait, mielőtt önállóan használhatja.<br> Ezt úgy teheti meg, hogy ellátogat <a href="https://www.strerr.com/en/md5_hash_chanager.html">ezen az oldalon</a>, majd újra letölti a képet. Ha ez továbbra sem működik, próbálja meg <a href="https://ezgif.com/resize">ezt az oldalt</a> és állítsa be 99%-os méretre.',
+        newReleasesTitle: "Új Kiadások:",
+        download: "Letöltés",
+        additionalText1: "Ez az oldal nem kereskedelmi célú és reklámmentes, és nyílt forráskódú.",
+        additionalText2: "Ezért az oldalon található összes kép igazságos használatra van licencelve. Ezeket a nyilvános domainből szereztük be.",
+        creditsLink: "Vissza a Főoldalra",
+        creatorsCreditsLink: "Készítők Kreditje"
+    },
+    ru: {
+        title: "Страница Создателей",
+        description: 'Декорации на этой странице предоставлены их художниками и, как таковые, являются более специальными и уникальными, чем коллекция на главной странице.<br> Помните, что из-за того, как Decor хранит свои файлы, вам нужно будет изменить метаданные каждой скачанной декорации, прежде чем вы сможете использовать её самостоятельно.<br> Вы можете сделать это, посетив <a href="https://www.strerr.com/en/md5_hash_chanager.html">этот сайт</a> и затем снова скачав изображение. Если это всё равно не работает, попробуйте <a href="https://ezgif.com/resize">этот сайт</a> и уменьшите изображение до 99% размера.',
+        newReleasesTitle: "Новые Выпуски:",
+        download: "Скачать",
+        additionalText1: "Этот сайт некоммерческий и без рекламы, а также является с открытым исходным кодом.",
+        additionalText2: "Таким образом, все изображения, размещенные на этом сайте, лицензированы для справедливого использования. Они были получены через общественное достояние.",
+        creditsLink: "Назад на Главную",
+        creatorsCreditsLink: "Кредиты Создателей"
+    }
+};
